7bbbb148f3635ee7764f98a2a1d4498739315391
pchchv <ipchchv@gmail.com>
2024-11-28 13:08:28 +0300

    Add flag parsing

--- cmd/goddns/goddns.go
+++ cmd/goddns/goddns.go
@@ -1,10 +1,25 @@
 package main
 
-import "github.com/pchchv/goddns/internal/utils"
+import (
+	"flag"
 
-// Version is current version of GoDDNS.
-var Version = "v0.1"
+	"github.com/fatih/color"
+	"github.com/pchchv/goddns/internal/utils"
+)
+
+var (
+	optHelp = flag.Bool("h", false, "Show help")
+	// Version is current version of GoDDNS.
+	Version = "v0.1"
+)
 
 func main() {
 	utils.Version = Version
+
+	flag.Parse()
+	if *optHelp {
+		color.Cyan(utils.Logo, Version)
+		flag.Usage()
+		return
+	}
 }
