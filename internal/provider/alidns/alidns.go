eb4a4cc6d6b5180fcfcf1781ee1f65ced9b8d524
pchchv <ipchchv@gmail.com>
2024-11-29 12:33:59 +0300

    Create a DomainRecord type

--- internal/provider/alidns/alidns.go
+++ internal/provider/alidns/alidns.go
@@ -1,3 +1,17 @@
+package alidns
+
+type DomainRecord struct {
+	DomainName string
+	RecordID   string `json:"RecordId"`
+	RR         string
+	Type       string
+	Value      string
+	Line       string
+	Priority   int
+	TTL        int
+	Status     string
+	Locked     bool
+}
 
 // AliDNS token.
 type AliDNS struct {
