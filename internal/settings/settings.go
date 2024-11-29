e4b7de593f9ca355d615ea8df972745ca0ac44f0
pchchv <ipchchv@gmail.com>
2024-11-29 11:47:49 +0300

    Create a Webhook and a WebPanel types

--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -4,3 +4,16 @@ type Domain struct {
 	DomainName string   `json:"domain_name" yaml:"domain_name"`
 	SubDomains []string `json:"sub_domains" yaml:"sub_domains"`
 }
+
+type Webhook struct {
+	Enabled     bool   `json:"enabled" yaml:"enabled"`
+	URL         string `json:"url" yaml:"url"`
+	RequestBody string `json:"request_body" yaml:"request_body"`
+}
+
+type WebPanel struct {
+	Enabled  bool   `json:"enabled" yaml:"enabled"`
+	Addr     string `json:"addr" yaml:"addr"`
+	Username string `json:"username" yaml:"username"`
+	Password string `json:"password" yaml:"password"`
+}
