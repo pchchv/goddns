fd269baa7ef6db3137c8e4f7ddd03d9b1003db5d
pchchv <ipchchv@gmail.com>
2024-11-29 12:23:57 +0300

    Add some consts

--- internal/utils/constants.go
+++ internal/utils/constants.go
@@ -1,4 +1,26 @@
 package utils
 
+const (
+	ALIDNS       = "AliDNS"
+	CLOUDFLARE   = "Cloudflare"
+	DIGITALOCEAN = "DigitalOcean"
+	DNSPOD       = "DNSPod" // dnspod.cn
+	DREAMHOST    = "Dreamhost"
+	DUCK         = "DuckDNS"
+	DYNU         = "Dynu"
+	DYNV6        = "Dynv6"
+	GOOGLE       = "Google"
+	HE           = "HE" // he.net
+	HETZNER      = "Hetzner"
+	INFOMANIAK   = "Infomaniak"
+	IONOS        = "IONOS"
+	LINODE       = "Linode"
+	LOOPIASE     = "LoopiaSE"
+	NOIP         = "NoIP"
+	OVH          = "OVH"
+	SCALEWAY     = "Scaleway"
+	STRATO       = "Strato"
+)
+
 // Version is current version of GoDNS.
 var Version = "v0.1"
