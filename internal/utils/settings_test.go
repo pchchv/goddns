82c3161a24deaa06fe5104e5c899c90314fcc9aa
pchchv <ipchchv@gmail.com>
2024-11-29 12:24:53 +0300

    Create tests of a CheckSettings function

---
+++ internal/utils/settings_test.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/pchchv/goddns/internal/settings"
+)
+
+func TestCheckSettings(t *testing.T) {
+	settingError := &settings.Settings{}
+	if err := CheckSettings(settingError); err == nil {
+		t.Error("setting is invalid, should return error")
+	}
+
+	settingDNSPod := &settings.Settings{Provider: "DNSPod", LoginToken: "aaa"}
+	if err := CheckSettings(settingDNSPod); err == nil {
+		t.Log("setting with login token, passed")
+	} else {
+		t.Error("setting with login token, should be passed")
+	}
+
+	settingDNSPod = &settings.Settings{Provider: "DNSPod"}
+	if err := CheckSettings(settingDNSPod); err == nil {
+		t.Error("setting with invalid parameters, should be failed")
+	}
+
+	settingHE := &settings.Settings{Provider: "HE", Password: ""}
+	if err := CheckSettings(settingHE); err != nil {
+		t.Log("HE setting without password, passed")
+	} else {
+		t.Error("HE setting without password, should be faild")
+	}
+}
