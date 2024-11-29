9129e4335eca242932422ebd2e4195d26817701d
pchchv <ipchchv@gmail.com>
2024-11-29 11:49:33 +0300

    Create a TelegramNotify and a DiscordNotify types

--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -25,3 +25,19 @@ type Mikrotik struct {
 	Password  string `json:"password" yaml:"password"`
 	Interface string `json:"interface" yaml:"interface"`
 }
+
+type TelegramNotify struct {
+	Enabled       bool   `json:"enabled" yaml:"enabled"`
+	BotAPIKey     string `json:"bot_api_key" yaml:"bot_api_key"`
+	BotAPIKeyFile string `json:"bot_api_key_file" yaml:"bot_api_key_file"`
+	ChatID        string `json:"chat_id" yaml:"chat_id"`
+	MsgTemplate   string `json:"message_template" yaml:"message_template"`
+}
+
+type DiscordNotify struct {
+	Enabled         bool   `json:"enabled" yaml:"enabled"`
+	BotAPIToken     string `json:"bot_api_token" yaml:"bot_api_token"`
+	BotAPITokenFile string `json:"bot_api_token_file" yaml:"bot_api_token_file"`
+	Channel         string `json:"channel" yaml:"channel"`
+	MsgTemplate     string `json:"message_template" yaml:"message_template"`
+}
