1ddf263570d55c58a85c56d23f1aadc0b43246c5
pchchv <ipchchv@gmail.com>
2024-11-30 19:58:12 +0300

    Implement a genRequestURL method

--- internal/provider/alidns/alidns.go
+++ internal/provider/alidns/alidns.go
@@ -1,13 +1,33 @@
 package alidns
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 )
 
+const baseURL = "https://alidns.aliyuncs.com/"
+
 var (
+	publicParam = map[string]string{
+		"AccessKeyId":      "",
+		"Format":           "JSON",
+		"Version":          "2015-01-09",
+		"SignatureMethod":  "HMAC-SHA1",
+		"Timestamp":        "",
+		"SignatureVersion": "1.0",
+		"SignatureNonce":   "",
+	}
 	instance *AliDNS
 	once     sync.Once
 )
@@ -54,6 +74,42 @@ func NewAliDNS(key, secret, ipType string) *AliDNS {
 		}
 	})
 	return instance
+}
+
+func (d *AliDNS) genRequestURL(params map[string]string) string {
+	var pArr []string
+	ps := map[string]string{}
+	for k, v := range publicParam {
+		ps[k] = v
+	}
+
+	for k, v := range params {
+		ps[k] = v
+	}
+
+	now := time.Now().UTC()
+	ps["AccessKeyId"] = d.AccessKeyID
+	ps["SignatureNonce"] = strconv.Itoa(int(now.UnixNano()) + rand.Intn(99999))
+	ps["Timestamp"] = now.Format("2006-01-02T15:04:05Z")
+
+	for k, v := range ps {
+		pArr = append(pArr, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	sort.Strings(pArr)
+	path := strings.Join(pArr, "&")
+
+	s := "GET&%2F&" + url.QueryEscape(path)
+	s = strings.Replace(s, "%3A", "%253A", -1)
+	s = strings.Replace(s, "%40", "%2540", -1)
+	s = strings.Replace(s, "%2A", "%252A", -1)
+	mac := hmac.New(sha1.New, []byte(d.AccessKeySecret+"&"))
+	if _, err := mac.Write([]byte(s)); err != nil {
+		return ""
+	}
+
+	sign := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+	return fmt.Sprintf("%s?%s&Signature=%s", baseURL, path, url.QueryEscape(sign))
 }
 
 func getHTTPBody(url string) ([]byte, error) {
