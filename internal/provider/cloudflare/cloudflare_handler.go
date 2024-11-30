ff675d14a4bc45f6fc92d8aa1ca86dc4534e0355
pchchv <ipchchv@gmail.com>
2024-11-30 20:09:08 +0300

    Create a DNSRecord type

---
+++ internal/provider/cloudflare/cloudflare_handler.go
@@ -0,0 +1,12 @@
+package cloudflare
+
+// DNSRecord for Cloudflare API.
+type DNSRecord struct {
+	ID      string `json:"id"`
+	IP      string `json:"content"`
+	Name    string `json:"name"`
+	Proxied bool   `json:"proxied"`
+	Type    string `json:"type"`
+	ZoneID  string `json:"zone_id"`
+	TTL     int32  `json:"ttl"`
+}
