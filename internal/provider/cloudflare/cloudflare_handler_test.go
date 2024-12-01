6f9b5e49b6077aed52d11f33c4a6e3b5684dd111
pchchv <ipchchv@gmail.com>
2024-12-01 09:24:14 +0300

    Create tests of updating DNS responce to JSON

--- internal/provider/cloudflare/cloudflare_handler_test.go
+++ internal/provider/cloudflare/cloudflare_handler_test.go
@@ -81,3 +81,48 @@ func TestDNSResponseToJSON(t *testing.T) {
 		t.Errorf("Name Error: %#v != example.com", resp.Records[0].Name)
 	}
 }
+
+func TestDNSUpdateResponseToJSON(t *testing.T) {
+	s := strings.NewReader(`
+    {
+        "result": {
+            "id": "F11cc63e02a42d38174b8e7c548a7b6f",
+            "type": "A",
+            "name": "example.com",
+            "content": "127.0.0.1",
+            "proxiable": true,
+            "proxied": true,
+            "ttl": 1,
+            "locked": false,
+            "zone_id": "mk2b6fa491c12445a4376666a32429e1",
+            "zone_name": "example.com",
+            "modified_on": "2018-10-12T14:29:53.205191Z",
+            "created_on": "2018-10-12T14:29:53.205191Z",
+            "meta": {
+              "auto_added": false,
+              "managed_by_apps": false,
+              "managed_by_argo_tunnel": false
+            }
+        },
+        "success": true,
+        "errors": [],
+        "messages": []
+    }`)
+
+	var resp DNSRecordUpdateResponse
+	if err := json.NewDecoder(s).Decode(&resp); err != nil {
+		t.Error(err.Error())
+	}
+
+	if resp.Success != true {
+		t.Errorf("Success Error: %#v != true ", resp.Success)
+	}
+
+	if resp.Record.ID != "F11cc63e02a42d38174b8e7c548a7b6f" {
+		t.Errorf("ID Error: %#v != F11cc63e02a42d38174b8e7c548a7b6f ", resp.Record.ID)
+	}
+
+	if resp.Record.Name != "example.com" {
+		t.Errorf("Name Error: %#v != example.com", resp.Record.Name)
+	}
+}
