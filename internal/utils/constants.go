471667eff56d7539ff6605177973209d3e5ffe40
pchchv <ipchchv@gmail.com>
2024-12-01 09:26:59 +0300

    Create a type ProviderSetting

--- internal/utils/constants.go
+++ internal/utils/constants.go
@@ -28,5 +28,16 @@ const (
 	RootDomain     = "@"
 )
 
+type ProviderSetting struct {
+	Name        string `json:"name" yaml:"name"`
+	Username    bool   `json:"username" yaml:"username"`
+	Email       bool   `json:"email" yaml:"email"`
+	Password    bool   `json:"password" yaml:"password"`
+	LoginToken  bool   `json:"login_token" yaml:"login_token"`
+	AppKey      bool   `json:"app_key" yaml:"app_key"`
+	AppSecret   bool   `json:"app_secret" yaml:"app_secret"`
+	ConsumerKey bool   `json:"consumer_key" yaml:"consumer_key"`
+}
+
 // Version is current version of GoDNS.
 var Version = "v0.1"
