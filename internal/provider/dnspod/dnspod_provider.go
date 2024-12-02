95cfd3a12b42fa8ae9b226ce78a5c829bf1eaf8f
pchchv <ipchchv@gmail.com>
2024-12-02 11:17:55 +0300

    Implement an Init method

--- internal/provider/dnspod/dnspod_provider.go
+++ internal/provider/dnspod/dnspod_provider.go
@@ -5,3 +5,7 @@ import "github.com/pchchv/goddns/internal/settings"
 type DNSProvider struct {
 	configuration *settings.Settings
 }
+
+func (provider *DNSProvider) Init(conf *settings.Settings) {
+	provider.configuration = conf
+}
