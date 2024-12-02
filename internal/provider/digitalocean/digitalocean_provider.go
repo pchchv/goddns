5d9633eff7b6d944601c616ca4bde27a4e82b31a
pchchv <ipchchv@gmail.com>
2024-12-02 05:18:14 +0300

    Implement an Init method

--- internal/provider/digitalocean/digitalocean_provider.go
+++ internal/provider/digitalocean/digitalocean_provider.go
@@ -2,7 +2,16 @@ package digitalocean
 
 import "github.com/pchchv/goddns/internal/settings"
 
+// URL is the endpoint for the DigitalOcean API.
+const URL = "https://api.digitalocean.com/v2"
+
 type DNSProvider struct {
 	configuration *settings.Settings
 	API           string
+}
+
+// Init passes DNS settings and store it to the provider instance.
+func (provider *DNSProvider) Init(conf *settings.Settings) {
+	provider.configuration = conf
+	provider.API = URL
 }
