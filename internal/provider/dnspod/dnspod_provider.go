f0e5d81ca54052c5808c8ec62b54f97cbb1dc8f3
pchchv <ipchchv@gmail.com>
2024-12-02 11:25:30 +0300

    Implement an updateIP method

--- internal/provider/dnspod/dnspod_provider.go
+++ internal/provider/dnspod/dnspod_provider.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
+	"github.com/bitly/go-simplejson"
 	"github.com/pchchv/goddns/internal/settings"
 	"github.com/pchchv/goddns/internal/utils"
 )
@@ -71,4 +73,45 @@ func (provider *DNSProvider) postData(url string, content url.Values) (string, e
 	resp, _ := io.ReadAll(response.Body)
 
 	return string(resp), nil
+}
+
+// updateIP update subdomain with current IP.
+func (provider *DNSProvider) updateIP(domainID int64, subDomainID string, subDomainName string, ip string) error {
+	value := url.Values{}
+	value.Add("domain_id", strconv.FormatInt(domainID, 10))
+	value.Add("record_id", subDomainID)
+	value.Add("sub_domain", subDomainName)
+
+	if strings.ToUpper(provider.configuration.IPType) == utils.IPV4 {
+		value.Add("record_type", utils.IPTypeA)
+	} else if strings.ToUpper(provider.configuration.IPType) == utils.IPV6 {
+		value.Add("record_type", utils.IPTypeAAAA)
+	} else {
+		log.Fatal("Must specify ip_type in config for DNSPod.")
+		return errors.New("must specify ip_type in config for DNSPod")
+	}
+
+	value.Add("record_line", "默认")
+	value.Add("value", ip)
+
+	response, err := provider.postData("/Record.Modify", value)
+	if err != nil {
+		log.Fatal("Failed to update record to new IP:", err)
+		return err
+	}
+
+	sjson, parseErr := simplejson.NewJson([]byte(response))
+	if parseErr != nil {
+		log.Fatal(parseErr)
+		return err
+	}
+
+	if sjson.Get("status").Get("code").MustString() == "1" {
+		log.Printf("New IP updated: %s", ip)
+	} else {
+		log.Fatalf("Failed to update IP record: %s", sjson.Get("status").Get("message").MustString())
+		return fmt.Errorf("failed to update IP record: %s", sjson.Get("status").Get("message").MustString())
+	}
+
+	return nil
 }
