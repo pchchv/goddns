72ab9070472b61e2f981d5c95960907a77c53eb5
pchchv <ipchchv@gmail.com>
2024-12-02 11:33:00 +0300

    Implement an Init method

--- internal/provider/dreamhost /dreamhost_handler.go
+++ internal/provider/dreamhost /dreamhost_handler.go
@@ -5,3 +5,8 @@ import "github.com/pchchv/goddns/internal/settings"
 type DNSProvider struct {
 	configuration *settings.Settings
 }
+
+// Init pass DNS settings and store it to the provider instance.
+func (provider *DNSProvider) Init(conf *settings.Settings) {
+	provider.configuration = conf
+}
