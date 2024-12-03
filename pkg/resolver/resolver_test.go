ee1c0d25f0bf89ec81ef828c4cf072f360ea42ce
pchchv <ipchchv@gmail.com>
2024-12-03 12:19:45 +0300

    Create tests of a LookupHost method

--- pkg/resolver/resolver_test.go
+++ pkg/resolver/resolver_test.go
@@ -1,8 +1,11 @@
 package resolver
 
 import (
+	"log"
 	"reflect"
 	"testing"
+
+	"github.com/miekg/dns"
 )
 
 func TestNew(t *testing.T) {
@@ -11,5 +14,15 @@ func TestNew(t *testing.T) {
 	resolver := New(servers)
 	if !reflect.DeepEqual(resolver.Servers, expectedServers) {
 		t.Error("resolver.Servers: ", resolver.Servers, "should be equal to", expectedServers)
+	}
+}
+
+func TestLookupHost_ValidServer(t *testing.T) {
+	resolver := New([]string{"8.8.8.8", "8.8.4.4"})
+	if result, err := resolver.LookupHost("google-public-dns-a.google.com", dns.TypeA); err != nil {
+		log.Println(err.Error())
+		t.Error("Should succeed dns lookup")
+	} else if result[0].String() != "8.8.8.8" {
+		t.Error("google-public-dns-a.google.com should be resolved to 8.8.8.8")
 	}
 }
