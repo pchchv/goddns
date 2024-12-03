f0b4fba82c8ea2e8d884ca6cd2c8a3dc2b548bf2
pchchv <ipchchv@gmail.com>
2024-12-03 12:12:23 +0300

    Implement a NewFromResolvConf function

--- pkg/resolver/resolver.go
+++ pkg/resolver/resolver.go
@@ -3,9 +3,13 @@
 package resolver
 
 import (
+	"errors"
 	"math/rand"
 	"net"
+	"os"
 	"time"
+
+	"github.com/miekg/dns"
 )
 
 type DNSResolver struct {
@@ -21,4 +25,23 @@ func New(servers []string) *DNSResolver {
 	}
 
 	return &DNSResolver{servers, len(servers) * 2, rand.New(rand.NewSource(time.Now().UnixNano()))}
+}
+
+// NewFromResolvConf initializes DnsResolver from resolv.conf like file.
+func NewFromResolvConf(path string) (*DNSResolver, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return &DNSResolver{}, errors.New("no such file or directory: " + path)
+	}
+
+	config, err := dns.ClientConfigFromFile(path)
+	if err != nil {
+		return &DNSResolver{}, err
+	}
+
+	var servers []string
+	for _, ipAddress := range config.Servers {
+		servers = append(servers, net.JoinHostPort(ipAddress, "53"))
+	}
+
+	return &DNSResolver{servers, len(servers) * 2, rand.New(rand.NewSource(time.Now().UnixNano()))}, err
 }
