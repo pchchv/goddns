38e431b30b383658ef7e2a971b7140cd8f08130e
pchchv <ipchchv@gmail.com>
2024-12-03 12:10:31 +0300

    Implement a New function

--- pkg/resolver/resolver.go
+++ pkg/resolver/resolver.go
@@ -2,10 +2,23 @@
 // based on miekg/dns
 package resolver
 
-import "math/rand"
+import (
+	"math/rand"
+	"net"
+	"time"
+)
 
 type DNSResolver struct {
 	Servers    []string
 	RetryTimes int
 	r          *rand.Rand
+}
+
+// New initializes DnsResolver.
+func New(servers []string) *DNSResolver {
+	for i := range servers {
+		servers[i] = net.JoinHostPort(servers[i], "53")
+	}
+
+	return &DNSResolver{servers, len(servers) * 2, rand.New(rand.NewSource(time.Now().UnixNano()))}
 }
