4a990126c08cbedd2a8ff13ade93031d806bc8ff
pchchv <ipchchv@gmail.com>
2024-12-04 11:43:08 +0300

    Implement a getData and getZoneID methods

--- internal/provider/hetzner/hetzner_handler.go
+++ internal/provider/hetzner/hetzner_handler.go
@@ -1,11 +1,16 @@
 package hetzner
 
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/pchchv/goddns/internal/settings"
 	"github.com/pchchv/goddns/internal/utils"
 )
+
+const BaseURL = "https://dns.hetzner.com/api/v1/" // API address
 
 type Record struct {
 	Type   string `json:"type"`
@@ -25,4 +30,50 @@ type DNSProvider struct {
 func (provider *DNSProvider) Init(conf *settings.Settings) {
 	provider.configuration = conf
 	provider.client = utils.GetHTTPClient(provider.configuration)
+}
+
+func (provider *DNSProvider) getData(endpoint string, param string, value string) ([]byte, error) {
+	req, _ := http.NewRequest("GET", BaseURL+endpoint, nil)
+	q := req.URL.Query()
+	q.Add(param, value)
+	req.URL.RawQuery = q.Encode()
+	req.Header.Add("Auth-API-Token", provider.configuration.LoginToken)
+	resp, err := provider.client.Do(req)
+	if err != nil {
+		log.Fatal("Error in fetching: ", err)
+		return nil, err
+	}
+
+	respBody, _ := io.ReadAll(resp.Body)
+	return respBody, nil
+}
+
+func (provider *DNSProvider) getZoneID(zoneName string) (string, error) {
+	type Zone struct {
+		ID string `json:"id"`
+	}
+
+	type GetAllZonesResponse struct {
+		Zones []Zone `json:"zones"`
+	}
+
+	respBody, err := provider.getData("zones", "name", zoneName)
+	if err != nil {
+		return "", err
+	}
+
+	response := GetAllZonesResponse{}
+	if err = json.Unmarshal(respBody, &response); err != nil {
+		return "", err
+	}
+
+	if len(response.Zones) == 0 {
+		return "", err
+	}
+
+	if len(response.Zones) > 1 {
+		return "", err
+	}
+
+	return response.Zones[0].ID, nil
 }
