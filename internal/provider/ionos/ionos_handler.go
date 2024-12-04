2f873383f886b8d062baa2f79fb2c0c313b8f59a
pchchv <ipchchv@gmail.com>
2024-12-04 12:18:40 +0300

    Implement a getData and a getZoneID methods

--- internal/provider/ionos/ionos_handler.go
+++ internal/provider/ionos/ionos_handler.go
@@ -1,11 +1,16 @@
 package ionos
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 
 	"github.com/pchchv/goddns/internal/settings"
 	"github.com/pchchv/goddns/internal/utils"
 )
+
+const BaseURL = "https://api.hosting.ionos.com/dns/v1/"
 
 type recordResponse struct {
 	ID       string `json:"id"`
@@ -38,4 +43,52 @@ type DNSProvider struct {
 func (provider *DNSProvider) Init(conf *settings.Settings) {
 	provider.configuration = conf
 	provider.client = utils.GetHTTPClient(provider.configuration)
+}
+
+func (provider *DNSProvider) getData(endpoint string, params map[string]string) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, BaseURL+endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("X-API-Key", provider.configuration.LoginToken)
+	if params != nil {
+		q := req.URL.Query()
+		for k, v := range params {
+			q.Add(k, v)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	resp, err := provider.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("failed to get data from " + BaseURL + endpoint + ", status code: " + resp.Status)
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
+
+func (provider *DNSProvider) getZoneID(domainName string) (string, error) {
+	body, err := provider.getData("zones", nil)
+	if err != nil {
+		return "", err
+	}
+
+	var zones []zoneResponse
+	if err = json.Unmarshal(body, &zones); err != nil {
+		return "", err
+	}
+
+	for _, zone := range zones {
+		if zone.Name == domainName {
+			return zone.ID, nil
+		}
+	}
+
+	return "", errors.New("zone " + domainName + " not found")
 }
