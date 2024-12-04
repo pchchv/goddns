53198d7a7facf13bf19ce8a92f9e1cb94c4bcc76
pchchv <ipchchv@gmail.com>
2024-12-04 11:37:30 +0300

    Create a DNSProvider type

--- internal/provider/hetzner/hetzner_handler.go
+++ internal/provider/hetzner/hetzner_handler.go
@@ -1,5 +1,11 @@
 package hetzner
 
+import (
+	"net/http"
+
+	"github.com/pchchv/goddns/internal/settings"
+)
+
 type Record struct {
 	Type   string `json:"type"`
 	ID     string `json:"id"`
@@ -7,4 +13,9 @@ type Record struct {
 	Value  string `json:"value"`
 	TTL    int64  `json:"ttl"`
 	ZoneID string `json:"zone_id"`
+}
+
+type DNSProvider struct {
+	configuration *settings.Settings
+	client        *http.Client
 }
