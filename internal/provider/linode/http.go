71589cfd585e84837527dc47d2620433ee51fa94
pchchv <ipchchv@gmail.com>
2024-12-04 12:42:13 +0300

    Implement an applyProxy function

--- internal/provider/linode/http.go
+++ internal/provider/linode/http.go
@@ -1,9 +1,12 @@
 package linode
 
 import (
+	"context"
 	"log"
+	"net"
 	"net/http"
 
+	"golang.org/x/net/proxy"
 	"golang.org/x/oauth2"
 )
 
@@ -17,4 +20,25 @@ func addBearerAuth(accessToken string, transport http.RoundTripper) http.RoundTr
 	log.Print("Using OAuth / API token to connect to DNS service")
 
 	return transportWithAuth
+}
+
+func applyProxy(proxyAddress string, transport *http.Transport) (*http.Transport, error) {
+	if proxyAddress == "" {
+		log.Print("Skipping proxy: proxy address is empty string")
+		return transport, nil
+	}
+
+	dialer, err := proxy.SOCKS5("tcp", proxyAddress, nil, proxy.Direct)
+	if err != nil {
+		return transport, err
+	}
+
+	log.Printf("Connected to proxy : %s", proxyAddress)
+
+	dialContext := func(_ context.Context, network, address string) (net.Conn, error) {
+		return dialer.Dial(network, address)
+	}
+
+	transport.DialContext = dialContext
+	return transport, nil
 }
