c153af010a1f2825c5c851957ab0a81b7d1ab180
pchchv <ipchchv@gmail.com>
2024-12-05 11:45:19 +0300

    Implement a getDomainID method

--- internal/provider/linode/provider.go
+++ internal/provider/linode/provider.go
@@ -1,6 +1,8 @@
 package linode
 
 import (
+	"context"
+	"errors"
 	"log"
 
 	"github.com/linode/linodego"
@@ -20,4 +22,25 @@ func (provider *DNSProvider) Init(conf *settings.Settings) {
 	linodeAPIClient := linodego.NewClient(httpClient)
 	linodeAPIClient.SetDebug(conf.DebugInfo)
 	provider.linodeClient = &linodeAPIClient
+}
+
+func (provider *DNSProvider) getDomainID(name string) (int, error) {
+	f := linodego.Filter{}
+	f.AddField(linodego.Eq, "domain", name)
+	fStr, err := f.MarshalJSON()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	opts := linodego.NewListOptions(0, string(fStr))
+	res, err := provider.linodeClient.ListDomains(context.Background(), opts)
+	if err != nil {
+		return 0, err
+	}
+
+	if len(res) == 0 {
+		return 0, errors.New("No domains found for name " + name)
+	}
+
+	return res[0].ID, nil
 }
