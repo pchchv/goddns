0ccd1faa633b9eb64c53ce8ee89ffbb4677c7274
pchchv <ipchchv@gmail.com>
2024-12-05 12:00:57 +0300

    Implement an Init method

--- internal/provider/noip/noip_handler.go
+++ internal/provider/noip/noip_handler.go
@@ -5,3 +5,8 @@ import "github.com/pchchv/goddns/internal/settings"
 type DNSProvider struct {
 	configuration *settings.Settings
 }
+
+// Init passes DNS settings and store it to provider instance.
+func (provider *DNSProvider) Init(conf *settings.Settings) {
+	provider.configuration = conf
+}
