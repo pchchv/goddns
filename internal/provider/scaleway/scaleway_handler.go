6a73a67d98dd719b45e8b62d37f6df343891c4c6
pchchv <ipchchv@gmail.com>
2024-12-05 12:11:13 +0300

    Create a Record type

--- internal/provider/scaleway/scaleway_handler.go
+++ internal/provider/scaleway/scaleway_handler.go
@@ -5,3 +5,10 @@ type IDFields struct {
 	Name string `json:"name"`
 	Type string `json:"type"`
 }
+
+type Record struct {
+	Name    string `json:"name"`
+	Data    string `json:"data"`
+	TTL     int    `json:"ttl"`
+	Comment string `json:"comment"`
+}
