2706f808a38ab2aa8cddac61bf40a6486ddcc6d2
pchchv <ipchchv@gmail.com>
2024-12-06 11:27:11 +0300

    Implement a NewPushoverNotification func

--- pkg/notification/pushover.go
+++ pkg/notification/pushover.go
@@ -5,3 +5,7 @@ import "github.com/pchchv/goddns/internal/settings"
 type PushoverNotification struct {
 	conf *settings.Settings
 }
+
+func NewPushoverNotification(conf *settings.Settings) INotification {
+	return &PushoverNotification{conf: conf}
+}
