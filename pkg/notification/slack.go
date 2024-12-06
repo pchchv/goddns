c0be0ccad99abf6f073dc0a590e4860a87470603
pchchv <ipchchv@gmail.com>
2024-12-06 11:24:36 +0300

    Implement a NewSlackNotification function

--- pkg/notification/slack.go
+++ pkg/notification/slack.go
@@ -5,3 +5,7 @@ import "github.com/pchchv/goddns/internal/settings"
 type SlackNotification struct {
 	conf *settings.Settings
 }
+
+func NewSlackNotification(conf *settings.Settings) INotification {
+	return &SlackNotification{conf: conf}
+}
