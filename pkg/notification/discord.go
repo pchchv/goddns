5a98f41b935cd7ad88b29fdd952c26c2e13a29d7
pchchv <ipchchv@gmail.com>
2024-12-06 11:19:28 +0300

    Implement a NewDiscordNotification func

--- pkg/notification/discord.go
+++ pkg/notification/discord.go
@@ -5,3 +5,7 @@ import "github.com/pchchv/goddns/internal/settings"
 type DiscordNotification struct {
 	conf *settings.Settings
 }
+
+func NewDiscordNotification(conf *settings.Settings) INotification {
+	return &DiscordNotification{conf: conf}
+}
