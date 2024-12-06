55fa99a8c55d8d0b665cce7eab352bb05fe382b2
pchchv <ipchchv@gmail.com>
2024-12-06 11:16:13 +0300

    Implement a NewTelegramNotification func

--- pkg/notification/telegram.go
+++ pkg/notification/telegram.go
@@ -5,3 +5,7 @@ import "github.com/pchchv/goddns/internal/settings"
 type TelegramNotification struct {
 	conf *settings.Settings
 }
+
+func NewTelegramNotification(conf *settings.Settings) INotification {
+	return &TelegramNotification{conf: conf}
+}
