7adcc6d605abedfe2513cf521531a1b91b81c169
pchchv <ipchchv@gmail.com>
2024-12-06 11:35:51 +0300

    Implement an initNotifications function

--- pkg/notification/manager.go
+++ pkg/notification/manager.go
@@ -1,6 +1,18 @@
 package notification
 
-import "log"
+import (
+	"log"
+
+	"github.com/pchchv/goddns/internal/settings"
+)
+
+const (
+	Email    = "email"
+	Slack    = "slack"
+	Discord  = "discord"
+	Telegram = "telegram"
+	Pushover = "pushover"
+)
 
 type INotification interface {
 	Send(domain, currentIP string) error
@@ -20,4 +32,29 @@ func (n *notificationManager) Send(domain, currentIP string) {
 			log.Fatalf("Send notification with error: %e", err)
 		}
 	}
+}
+
+func initNotifications(conf *settings.Settings) map[string]INotification {
+	notificationMap := map[string]INotification{}
+	if conf.Notify.Mail.Enabled {
+		notificationMap[Email] = NewEmailNotification(conf)
+	}
+
+	if conf.Notify.Telegram.Enabled {
+		notificationMap[Telegram] = NewTelegramNotification(conf)
+	}
+
+	if conf.Notify.Discord.Enabled {
+		notificationMap[Discord] = NewDiscordNotification(conf)
+	}
+
+	if conf.Notify.Slack.Enabled {
+		notificationMap[Slack] = NewSlackNotification(conf)
+	}
+
+	if conf.Notify.Pushover.Enabled {
+		notificationMap[Pushover] = NewPushoverNotification(conf)
+	}
+
+	return notificationMap
 }
