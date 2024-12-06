671cb65dc58d0de7e03af896fa9cd92aa99b73e4
pchchv <ipchchv@gmail.com>
2024-12-06 11:18:00 +0300

    Implement a method Send

--- pkg/notification/telegram.go
+++ pkg/notification/telegram.go
@@ -1,6 +1,15 @@
 package notification
 
-import "github.com/pchchv/goddns/internal/settings"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/pchchv/goddns/internal/settings"
+	"github.com/pchchv/goddns/internal/utils"
+)
 
 type TelegramNotification struct {
 	conf *settings.Settings
@@ -8,4 +17,58 @@ type TelegramNotification struct {
 
 func NewTelegramNotification(conf *settings.Settings) INotification {
 	return &TelegramNotification{conf: conf}
+}
+
+func (n *TelegramNotification) Send(domain, currentIP string) (err error) {
+	if n.conf.Notify.Telegram.BotAPIKey == "" {
+		return errors.New("bot api key cannot be empty")
+	}
+
+	if n.conf.Notify.Telegram.ChatID == "" {
+		return errors.New("chat id cannot be empty")
+	}
+
+	client := utils.GetHTTPClient(n.conf)
+	tpl := n.conf.Notify.Telegram.MsgTemplate
+	if tpl == "" {
+		tpl = "_Your IP address is changed to_%0A%0A*{{ .CurrentIP }}*%0A%0ADomain *{{ .Domain }}* updated"
+	}
+
+	var response *http.Response
+	msg := buildTemplate(currentIP, domain, tpl)
+	reqURL := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&parse_mode=Markdown&text=%s",
+		n.conf.Notify.Telegram.BotAPIKey,
+		n.conf.Notify.Telegram.ChatID,
+		msg)
+	response, err = client.Get(reqURL)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	body, _ := io.ReadAll(response.Body)
+	type ResponseParameters struct {
+		MigrateToChatID int64 `json:"migrate_to_chat_id"` // optional
+		RetryAfter      int   `json:"retry_after"`        // optional
+	}
+
+	type APIResponse struct {
+		Ok          bool                `json:"ok"`
+		Result      json.RawMessage     `json:"result"`
+		ErrorCode   int                 `json:"error_code"`
+		Description string              `json:"description"`
+		Parameters  *ResponseParameters `json:"parameters"`
+	}
+
+	var resp APIResponse
+	if err = json.Unmarshal(body, &resp); err != nil {
+		fmt.Println("error:", err)
+		return errors.New("failed to parse response")
+	}
+
+	if !resp.Ok {
+		return errors.New(resp.Description)
+	}
+
+	return nil
 }
