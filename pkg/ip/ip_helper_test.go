403c360c3727fa1b590278b6e00fbb0eed39b67b
pchchv <ipchchv@gmail.com>
2024-12-07 20:07:19 +0300

    Create a test of a getting Mikrotik IP

---
+++ pkg/ip/ip_helper_test.go
@@ -0,0 +1,27 @@
+package ip_test
+
+import (
+	"testing"
+
+	"github.com/pchchv/goddns/internal/settings"
+	"github.com/pchchv/goddns/pkg/ip"
+)
+
+func TestGetMikrotikIP(t *testing.T) {
+	t.Skip()
+	conf := &settings.Settings{
+		Mikrotik: settings.Mikrotik{
+			Enabled:   true,
+			Addr:      "http://192.168.20.1:81",
+			Username:  "admin",
+			Password:  "",
+			Interface: "pppoe-out",
+		},
+	}
+	helper := ip.GetIPHelperInstance(conf)
+	if ip := helper.GetCurrentIP(); ip == "" {
+		t.Log("IP is empty...")
+	} else {
+		t.Log("IP is:" + ip)
+	}
+}
