65c4fc85975e8c378dbf1541a88f6eb6c29a8cfe
pchchv <ipchchv@gmail.com>
2024-12-07 19:58:40 +0300

    Implement a method getIPOnline

--- internal/utils/constants.go
+++ internal/utils/constants.go
@@ -15,17 +15,27 @@ const (
 	HETZNER        = "Hetzner"
 	INFOMANIAK     = "Infomaniak"
 	IONOS          = "IONOS"
+	IPPattern      = "(" + IPv4Pattern + ")|(" + IPv6Pattern + ")" // regex pattern to match IPV4 and IPV6 address.
 	IPTypeA        = "A"
 	IPTypeAAAA     = "AAAA"
 	IPV4           = "IPV4"
+	IPv4Pattern    = `((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)` // regex pattern to match IPV4 address
 	IPV6           = "IPV6"
-	LINODE         = "Linode"
-	LOOPIASE       = "LoopiaSE"
-	NOIP           = "NoIP"
-	OVH            = "OVH"
-	SCALEWAY       = "Scaleway"
-	STRATO         = "Strato"
-	RootDomain     = "@"
+	IPv6Pattern    = `(([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|` + // regex pattern to match IPV6 address
+		`(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|` +
+		`(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|` +
+		`(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|` +
+		`(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|` +
+		`(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|` +
+		`(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|` +
+		`(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))`
+	LINODE     = "Linode"
+	LOOPIASE   = "LoopiaSE"
+	NOIP       = "NoIP"
+	OVH        = "OVH"
+	SCALEWAY   = "Scaleway"
+	STRATO     = "Strato"
+	RootDomain = "@"
 )
 
 type ProviderSetting struct {
--- pkg/ip/ip_helper.go
+++ pkg/ip/ip_helper.go
@@ -125,6 +125,78 @@ func (helper *IPHelper) getNext() string {
 	return helper.reqURLs[newIdx]
 }
 
+// getIPOnline gets public IP from internet.
+func (helper *IPHelper) getIPOnline() (onlineIP string) {
+	transport := &http.Transport{
+		DialContext: func(ctx context.Context, _, addr string) (net.Conn, error) {
+			proto := "tcp"
+			if strings.ToUpper(helper.configuration.IPType) == utils.IPV4 {
+				// Force the network to "tcp4" to use only IPv4
+				proto = "tcp4"
+			}
+
+			return (&net.Dialer{
+				Timeout:   time.Second * utils.DefaultTimeout,
+				KeepAlive: 30 * time.Second,
+			}).DialContext(ctx, proto, addr)
+		},
+	}
+	client := &http.Client{
+		Timeout:   time.Second * utils.DefaultTimeout,
+		Transport: transport,
+	}
+	for {
+		reqURL := helper.getNext()
+		req, _ := http.NewRequest("GET", reqURL, nil)
+		if helper.configuration.UserAgent != "" {
+			req.Header.Set("User-Agent", helper.configuration.UserAgent)
+		}
+
+		response, err := client.Do(req)
+		if err != nil {
+			log.Fatal("Cannot get IP:", err)
+			time.Sleep(time.Millisecond * 300)
+			continue
+		}
+
+		if response.StatusCode != http.StatusOK {
+			log.Fatal(fmt.Sprintf("request %v got httpCode:%v", reqURL, response.StatusCode))
+			continue
+		}
+
+		body, _ := io.ReadAll(response.Body)
+		ipReg := regexp.MustCompile(utils.IPPattern)
+		onlineIP = ipReg.FindString(string(body))
+		if onlineIP == "" {
+			log.Fatal(fmt.Sprintf("request:%v failed to get online IP", reqURL))
+			continue
+		}
+
+		if isIPv4(onlineIP) && strings.ToUpper(helper.configuration.IPType) != utils.IPV4 {
+			log.Fatalf("The online IP (%s) from %s is not IPV6, will skip it.", onlineIP, reqURL)
+			continue
+		} else if strings.ToUpper(helper.configuration.IPType) != utils.IPV6 {
+			log.Fatalf("The online IP (%s) from %s is not IPV4, will skip it.", onlineIP, reqURL)
+			continue
+		}
+
+		log.Printf("Get ip success by: %s, online IP: %s", reqURL, onlineIP)
+
+		if err = response.Body.Close(); err != nil {
+			log.Fatal(fmt.Sprintf("request:%v failed to get online IP", reqURL))
+			continue
+		}
+
+		if onlineIP == "" {
+			log.Fatal("fail to get online IP")
+		}
+
+		break
+	}
+
+	return
+}
+
 func isIPv4(ip string) bool {
 	return strings.Count(ip, ":") < 2
 }
