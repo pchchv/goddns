7ef10fd29250c638c6041b073373acaad52b98f1
pchchv <ipchchv@gmail.com>
2024-12-07 19:42:23 +0300

    Implement an UpdateConfiguration method

--- pkg/ip/ip_helper.go
+++ pkg/ip/ip_helper.go
@@ -1,3 +1,13 @@
+package ip
+
+import (
+	"log"
+	"strings"
+	"sync"
+
+	"github.com/pchchv/goddns/internal/settings"
+	"github.com/pchchv/goddns/internal/utils"
+)
 
 type IPHelper struct {
 	reqURLs       []string
@@ -5,4 +15,40 @@ type IPHelper struct {
 	mutex         sync.RWMutex
 	configuration *settings.Settings
 	idx           int64
+}
+
+func (helper *IPHelper) UpdateConfiguration(conf *settings.Settings) {
+	helper.mutex.Lock()
+	defer helper.mutex.Unlock()
+
+	// clear urls
+	helper.reqURLs = helper.reqURLs[:0]
+	// reset the index
+	helper.idx = -1
+
+	if conf.IPType == "" || strings.ToUpper(conf.IPType) == utils.IPV4 {
+		// filter empty urls
+		for _, url := range conf.IPUrls {
+			if url != "" {
+				helper.reqURLs = append(helper.reqURLs, url)
+			}
+		}
+
+		if conf.IPUrl != "" {
+			helper.reqURLs = append(helper.reqURLs, conf.IPUrl)
+		}
+	} else {
+		// filter empty urls
+		for _, url := range conf.IPV6Urls {
+			if url != "" {
+				helper.reqURLs = append(helper.reqURLs, url)
+			}
+		}
+
+		if conf.IPV6Url != "" {
+			helper.reqURLs = append(helper.reqURLs, conf.IPV6Url)
+		}
+	}
+
+	log.Printf("Update ip helper configuration, urls: %v", helper.reqURLs)
 }
