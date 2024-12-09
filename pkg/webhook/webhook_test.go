195161dfd6fce8ceb8cb0b9082aebe92a3e1b64c
pchchv <ipchchv@gmail.com>
2024-12-09 11:41:32 +0300

    Create a test for building a request URL

--- pkg/webhook/webhook_test.go
+++ pkg/webhook/webhook_test.go
@@ -26,3 +26,21 @@ func TestBuildReqBody(t *testing.T) {
 
 	t.Log(ret)
 }
+
+func TestBuildReqURL(t *testing.T) {
+	w := GetWebhook(&settings.Settings{
+		Webhook: settings.Webhook{
+			Enabled: true,
+			URL:     "http://localhost:5000/api/v1/send?domain={{.Domain}}&ip={{.CurrentIP}}&ip_type={{.IPType}}",
+		}})
+	ret, err := w.buildReqURL("example.com", "192.168.1.1", utils.IPV4)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if expected := "http://localhost:5000/api/v1/send?domain=example.com&ip=192.168.1.1&ip_type=IPV4"; ret != expected {
+		t.Errorf("expected %s, got %s", expected, ret)
+	}
+
+	t.Log(ret)
+}
