f5ff0f3743fc781c61e5be441d5c296e6fcef6c1
pchchv <ipchchv@gmail.com>
2024-12-09 11:40:51 +0300

    Create a test for building a request body

---
+++ pkg/webhook/webhook_test.go
@@ -0,0 +1,28 @@
+package webhook
+
+import (
+	"testing"
+
+	"github.com/pchchv/goddns/internal/settings"
+	"github.com/pchchv/goddns/internal/utils"
+)
+
+func TestBuildReqBody(t *testing.T) {
+	t.Skip()
+	w := GetWebhook(&settings.Settings{
+		Webhook: settings.Webhook{
+			Enabled:     true,
+			URL:         "http://localhost:5000/api/v1/send",
+			RequestBody: "{ \"domain\": \"{{.Domain}}\", \"ip\": \"{{.CurrentIP}}\", \"ip_type\": \"{{.IPType}}\" }",
+		}})
+	ret, err := w.buildReqBody("example.com", "192.168.1.1", utils.IPV4)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if expected := `{ "domain": "example.com", "ip": "192.168.1.1", "ip_type": "IPV4" }`; ret != expected {
+		t.Errorf("expected %s, got %s", expected, ret)
+	}
+
+	t.Log(ret)
+}
