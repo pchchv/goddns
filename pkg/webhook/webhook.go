68f0568c7095073e57ec6c37fa2c20d7e64f6fa2
pchchv <ipchchv@gmail.com>
2024-12-09 11:35:23 +0300

    Implement a method Execute

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -2,8 +2,10 @@ package webhook
 
 import (
 	"bytes"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 	"sync"
 	"text/template"
 
@@ -19,6 +21,60 @@ var (
 type Webhook struct {
 	conf   *settings.Settings
 	client *http.Client
+}
+
+func (w *Webhook) Execute(domain, currentIP string) (err error) {
+	if w.conf.Webhook.URL == "" {
+		log.Print("Webhook URL is empty, skip sending notification")
+		return nil
+	}
+
+	// set request method
+	method := http.MethodGet
+	if w.conf.Webhook.RequestBody != "" {
+		method = http.MethodPost
+	}
+
+	// send HTTP get request
+	var reqURL, reqBody string
+	if method == http.MethodGet {
+		reqURL, err = w.buildReqURL(domain, currentIP, w.conf.IPType)
+		if err != nil {
+			return err
+		}
+	} else {
+		reqURL = w.conf.Webhook.URL
+		reqBody, err = w.buildReqBody(domain, currentIP, w.conf.IPType)
+		if err != nil {
+			return err
+		}
+	}
+
+	var req *http.Request
+	if req, err = http.NewRequest(method, reqURL, strings.NewReader(reqBody)); err != nil {
+		log.Fatal("Failed to create request:", err)
+		return err
+	}
+
+	if method == http.MethodPost {
+		req.Header.Add("Content-Type", "application/json")
+	}
+
+	resp, err := w.client.Do(req)
+	if err != nil {
+		log.Fatal("Failed to send request:", err)
+		return err
+	}
+
+	defer resp.Body.Close()
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Failed to read response body:", err)
+		return err
+	}
+
+	log.Printf("Webhook response: %s", string(content))
+	return nil
 }
 
 func (w *Webhook) buildReqBody(domain, currentIP, ipType string) (string, error) {
