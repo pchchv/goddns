ac6ff1f72fe169293d891ec8d4052eac92eb2ffa
pchchv <ipchchv@gmail.com>
2024-12-09 11:33:36 +0300

    Implement a buildReqBody method

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -1,8 +1,11 @@
 package webhook
 
 import (
+	"bytes"
+	"log"
 	"net/http"
 	"sync"
+	"text/template"
 
 	"github.com/pchchv/goddns/internal/settings"
 	"github.com/pchchv/goddns/internal/utils"
@@ -16,6 +19,32 @@ var (
 type Webhook struct {
 	conf   *settings.Settings
 	client *http.Client
+}
+
+func (w *Webhook) buildReqBody(domain, currentIP, ipType string) (string, error) {
+	t := template.New("reqBody template")
+	if _, err := t.Parse(w.conf.Webhook.RequestBody); err != nil {
+		log.Fatal("Failed to parse template:", err)
+		return "", err
+	}
+
+	data := struct {
+		CurrentIP string
+		Domain    string
+		IPType    string
+	}{
+		currentIP,
+		domain,
+		ipType,
+	}
+
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, data); err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	return tpl.String(), nil
 }
 
 func GetWebhook(conf *settings.Settings) *Webhook {
