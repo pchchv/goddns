ac3c3915b59c3f7a254e4b4c8f834890107bd4e0
pchchv <ipchchv@gmail.com>
2024-12-09 11:47:32 +0300

    Implement an updateDNS method

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,11 +2,22 @@ package handler
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
 
 	"github.com/pchchv/goddns/internal/provider"
 	"github.com/pchchv/goddns/internal/settings"
+	"github.com/pchchv/goddns/internal/utils"
 	"github.com/pchchv/goddns/pkg/ip"
 	"github.com/pchchv/goddns/pkg/notification"
+	"github.com/pchchv/goddns/pkg/webhook"
+)
+
+var (
+	errEmptyResult = errors.New("empty result")
+	errEmptyDomain = errors.New("NXDOMAIN")
 )
 
 type Handler struct {
@@ -34,4 +45,47 @@ func (handler *Handler) SetProvider(provider provider.IDNSProvider) {
 
 func (handler *Handler) SetContext(ctx context.Context) {
 	handler.ctx = ctx
+}
+
+func (handler *Handler) updateDNS(domain *settings.Domain, ip string) error {
+	var updatedDomains []string
+	for _, subdomainName := range domain.SubDomains {
+		var hostname string
+		if subdomainName != utils.RootDomain {
+			hostname = subdomainName + "." + domain.DomainName
+		} else {
+			hostname = domain.DomainName
+		}
+
+		lastIP, err := utils.ResolveDNS(hostname, handler.Configuration.Resolver, handler.Configuration.IPType)
+		if err != nil && (errors.Is(err, errEmptyResult) || errors.Is(err, errEmptyDomain)) {
+			log.Fatalf("Failed to resolve DNS for domain: %s, error: %s", hostname, err)
+			continue
+		}
+
+		// check against the current known IP, if no change, skip update
+		if ip == lastIP {
+			log.Printf("IP is the same as cached one (%s). Skip update.", ip)
+		} else {
+			if err := handler.dnsProvider.UpdateIP(domain.DomainName, subdomainName, ip); err != nil {
+				return err
+			}
+
+			updatedDomains = append(updatedDomains, subdomainName)
+
+			// execute webhook when it is enabled
+			if handler.Configuration.Webhook.Enabled {
+				if err := webhook.GetWebhook(handler.Configuration).Execute(hostname, ip); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	if len(updatedDomains) > 0 {
+		successMessage := fmt.Sprintf("[ %s ] of %s", strings.Join(updatedDomains, ", "), domain.DomainName)
+		handler.notificationManager.Send(successMessage, ip)
+	}
+
+	return nil
 }
