bfc5334b1b96b09e8d35b7b3d2205163afcb604d
pchchv <ipchchv@gmail.com>
2024-12-09 11:31:32 +0300

    Implement a GetWebhook function

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -2,11 +2,29 @@ package webhook
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/pchchv/goddns/internal/settings"
+	"github.com/pchchv/goddns/internal/utils"
+)
+
+var (
+	instance *Webhook
+	once     sync.Once
 )
 
 type Webhook struct {
 	conf   *settings.Settings
 	client *http.Client
+}
+
+func GetWebhook(conf *settings.Settings) *Webhook {
+	once.Do(func() {
+		instance = &Webhook{
+			conf:   conf,
+			client: utils.GetHTTPClient(conf),
+		}
+	})
+
+	return instance
 }
