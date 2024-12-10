e03466ef25bcd3c11c5d7bb328bc65fbf6aea654
pchchv <ipchchv@gmail.com>
2024-12-10 11:02:48 +0300

    Implement a method startServer

--- internal/manager/dns_manager.go
+++ internal/manager/dns_manager.go
@@ -2,6 +2,7 @@ package manager
 
 import (
 	"context"
+	"log"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/pchchv/goddns/internal/handler"
@@ -20,4 +21,31 @@ type DNSManager struct {
 	server      *server.Server
 	configPath  string
 	defaultAddr string
+}
+
+func (manager *DNSManager) startServer() {
+	// start the internal HTTP server
+	if (manager.config.WebPanel.Addr != "" || manager.defaultAddr != ":9000") && manager.config.WebPanel.Enabled {
+		manager.server = &server.Server{}
+		var addr string
+		if manager.config.WebPanel.Addr != "" {
+			addr = manager.config.WebPanel.Addr
+		} else {
+			addr = manager.defaultAddr
+		}
+		manager.server.
+			SetAddress(addr).
+			SetAuthInfo(manager.config.WebPanel.Username, manager.config.WebPanel.Password).
+			SetConfig(manager.config).
+			SetConfigPath(manager.configPath).
+			Build()
+
+		go func() {
+			if err := manager.server.Start(); err != nil {
+				log.Fatalf("Failed to start the web server, error:%v", err)
+			}
+		}()
+	} else {
+		log.Println("Web panel is disabled")
+	}
 }
